06962547a7d287066fcef44a1b16d28a73ece464
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-03-27 18:28:23 +0900

    Some fix

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 import { Transform } from "stream";
 import iconv from "iconv-lite";
 
 class EncodeStream extends Transform {
   _transform(chunk, _1, callback) {
-    this.push(iconv.decode(chunk, "Shift_JIS"));
+    this.push(iconv.encode(iconv.decode(chunk, "Shift_JIS"), "UTF-8"));
     callback();
   }
 }
